6a64af6ef7a2fb93734b95d378d038b5b4ba2e40
Chandan Kumar <47857139+chandan73933@users.noreply.github.com>
2021-06-13 12:13:04 +0530

    Add files via upload

---
+++ binnary.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,m,k,i,j,s;
+	cin>>n>>m;
+	int a[n],b[m],p[100]={};
+	for(i=0;i<n;i++)
+	cin>>a[i];
+	for(j=0;j<m;j++)
+	cin>>b[j];
+		for(int p=0;p<k;p++)
+		{
+		if(a[i]&&b[j]==0)
+		{
+		
+		cout<<(p[]=a[i]+b[j]);
+		}
+		else if(a[i]&&b[j]==1)
+		{
+		cout<<(p[]=a[i]+b[j]);
+		}
+		else
+		{
+		cout<<"not aa binary";
+		}
+		}
+		
+		
+}
