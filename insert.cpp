21a991c4f3d1a526d4b055d0ae5548ed2d66c58b
Chandan Kumar <47857139+chandan73933@users.noreply.github.com>
2021-06-13 12:16:47 +0530

    Add files via upload

---
+++ insert.cpp
@@ -0,0 +1,24 @@
+// program to insert the element atr the end?
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int i;
+int a[6],elem;
+cout<<"\n enter the 5 element:-";
+for(i=0;i<5;i++)
+{
+cin>>a[i];
+}
+cout<<"\n the element is:-";
+cin>>elem;
+a[i]=elem;
+for(int i=0;i<6;i++)
+{
+cout<<"\n"<<a[i];
+}
+
+}
+	
