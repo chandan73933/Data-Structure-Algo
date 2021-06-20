8b703e4e75eb245a52881713e09b8e5311f3eb38
Chandan Kumar <47857139+chandan73933@users.noreply.github.com>
2021-06-20 10:50:26 +0530

    Add files via upload

---
+++ positive.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+        if(a[i]>0)
+        { 
+        	
+           
+        
+        cout<<sizeof(i);
+    }
+    }
+
